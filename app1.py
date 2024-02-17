39abe6d6dec700381f0e5279e3c3ae8fd6687063
GeniusMundi10 <159649324+GeniusMundi10@users.noreply.github.com>
2024-02-17 15:08:43 +0530

    Adding Login Screen

--- app1.py
+++ app1.py
@@ -10,6 +10,15 @@ load_dotenv()
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
+# Hardcoded credentials
+hardcoded_username = "your_username"
+hardcoded_password = "your_password"
+
+# Function to authenticate user
+def authenticate(username, password):
+    return username == hardcoded_username and password == hardcoded_password
+
+
 
 st.set_page_config(page_title="Prototype Chatbot")
 st.title("ELT CORPORATE PRIVATE GPT")
@@ -22,27 +31,35 @@ def get_gemini_response(question):
 	return {
 		"chunks": [response] if isinstance(response, str) else [chunk.text for chunk in response]
 	}
+#Login Section
+username = st.text_input("Username:")
+password = st.text_input("Password:", type="password")
+login_button = st.button("Login")
 
+if login_button:
+	if authenticate(username,password):
+	# Only show chat interface if login is successful
+		if 'chat_history' not in st.session_state:
+			st.session_state['chat_history']=[]
 
-if 'chat_history' not in st.session_state:
-	st.session_state['chat_history']=[]
+		input_question = st.text_input("Input: ",key="input")
+		submit_button = st.button("Ask the question")
 
-input_question = st.text_input("Input: ",key="input")
-submit_button = st.button("Ask the question")
+		if submit_button and input_question:
+			response = get_gemini_response(input_question)
+			st.session_state['chat_history'].append(("You: ",input_question))
+			bot_response=' '.join(response["chunks"])
 
-if submit_button and input_question:
-	response = get_gemini_response(input_question)
-	st.session_state['chat_history'].append(("You: ",input_question))
-	bot_response=' '.join(response["chunks"])
-
-	st.subheader("The Response is")
+			st.subheader("The Response is")
 	
-	st.markdown(bot_response,unsafe_allow_html=True)
-	st.session_state['chat_history'].append(("ELT-GPT: ",bot_response))
+			st.markdown(bot_response,unsafe_allow_html=True)
+			st.session_state['chat_history'].append(("ELT-GPT: ",bot_response))
 
-st.subheader("The Chat History is")
+		st.subheader("The Chat History is")
 
-chat_history_text = ""
-for role,text in st.session_state['chat_history']:
-	chat_history_text += f"{role}: {text}\n"
-st.text_area("Chat History", value=chat_history_text,height=400)
+		chat_history_text = ""
+		for role,text in st.session_state['chat_history']:
+			chat_history_text += f"{role}: {text}\n"
+		st.text_area("Chat History", value=chat_history_text,height=400)
+	else:
+		st.error("Invalid credentials. Please try again.")
